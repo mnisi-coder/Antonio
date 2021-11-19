7758ae578f50488386d15dfd122f07047ba7e38f
Jali Mnisi <mnisi@thedigitalacademy.co.za>
2021-11-19 18:00:54 +0200

    last push

--- database/bus.sql
+++ database/bus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2021 at 08:58 PM
+-- Generation Time: Nov 19, 2021 at 05:00 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `bus`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `staffNo` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `staffNo`, `name`, `email`, `password`, `date`) VALUES
+(1, '991143', 'Jali', 'mdawe@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-11-15');
 
 -- --------------------------------------------------------
 
@@ -45,11 +67,12 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`id`, `stdNumber`, `email`, `name`, `origin`, `bookingDate`, `time`, `destination`, `reference`, `status`) VALUES
-(1, '216448154', 'JAli@gmail.com', 'JAli Mnisi', 'Arcadia Campus', '2021-11-09', '09', 'Arcadia Campus', '', ''),
-(2, '216448154', 'JAli@gmail.com', 'JAli Mnisi', 'Arcadia Campus', '2021-11-09', '09', 'Arcadia Campus', '2164481541636231904JAli@gmail.com', 'Pending'),
+(1, '216448155', 'JAli@gmail.com', 'JAli Mnisi', 'Arcadia Campus', '2021-11-09', '09', 'Arcadia Campus', '', ''),
+(2, '216448154', 'JAli@gmail.com', 'JAli Mnisi', 'Arcadia Campus', '2021-11-09', '09', 'Arcadia Campus', '2164481541636231904JAli@gmail.com', 'Boarded'),
 (3, '216448154', 'JAli@gmail.com', 'JAli Mnisi', 'Soshanguve', '2021-11-10', '09', 'Arcadia Campus', '21644815416362333002021-11-06', 'Pending'),
-(4, '216448154', 'JAli@gmail.com', 'JAli Mnisi', 'Arcadia Campus', '2021-11-13', '17', 'Soshanguve', '21644815416362335012021-11-06', 'Pending'),
-(5, '216448154', 'mnisi@gmail.com', 'jali', 'Arcadia Campus', '2021-11-17', '16', 'Soshanguve', '21644815416368378582021-11-13', 'Pending');
+(4, '216448154', 'JAli@gmail.com', 'JAli Mnisi', 'Arcadia Campus', '2021-11-13', '17', 'Soshanguve', '21644815416362335012021-11-06', 'CANCELLED'),
+(5, '216448154', 'mnisi@gmail.com', 'jali', 'Arcadia Campus', '2021-11-17', '16', 'Soshanguve', '21644815416368378582021-11-13', 'Pending'),
+(6, '216448154', 'mnisi@gmail.com', 'jalo', 'Arcadia Campus', '2021-11-18', '21', 'Soshanguve', '21644815416372654842021-11-18', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -71,11 +94,19 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `stdNumber`, `name`, `email`, `password`, `date`) VALUES
-(1, '216448154', 'jali', 'mnisi@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-11-13');
+(1, '216448154', 'jalo', 'mnisi@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-11-13'),
+(2, '216457551', 'Malondana', 'londi@gmail.com', 'b2f21823cb50a9b3b70447736f1ea616', '2021-11-15'),
+(3, '123456789', 'dolo', 'jali@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-11-15');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `booking`
@@ -94,16 +125,22 @@ ALTER TABLE `student`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
